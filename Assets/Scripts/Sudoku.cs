fb715dfe74bf9c0616f0b2506a651207c2493638
WelbyC <welby.c@yahoo.com>
2018-11-29 10:27:45 -0800

    Add files via upload

---
+++ Assets/Scripts/Sudoku.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sudoku : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        Destroy(gameObject, 3.0f);
+	}
+}
